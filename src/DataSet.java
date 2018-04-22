f5dfcedc3dc53bf0ca147279a4e2a79694c0d3c6
TORESORLUBYCAKD <3326017670@qq.com>
2018-04-22 14:04:45 +0800

    update DataSet

--- src/DataSet.java
+++ src/DataSet.java
@@ -4,17 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Table;
+
 import gzl.sgd.math.SparseMatrix;
 
 
 public class DataSet {
 
 	public static int userNumber=943;
-	public static int itemNumber=1330;
+	public static int itemNumber=1440;
 	
 	
     public static SparseMatrix readDataSet(String file) throws FileNotFoundException {
-    	SparseMatrix dataset = new SparseMatrix(userNumber,itemNumber);
+    	SparseMatrix dataset;
+    	Table<Integer, Integer, Double> dataTable = HashBasedTable.create();
+    	Multimap<Integer, Integer> colMap = HashMultimap.create();
         Scanner scanner = new Scanner(new File(file));
         while(scanner.hasNextLine()) {
             String line = scanner.nextLine();
@@ -30,8 +37,11 @@ public class DataSet {
             double label = Double.parseDouble(columns[2]);
             //Instance instance = new Instance(label, data);
             //System.out.println(" user: "+data[0]+" item: "+data[1]+" rating: "+data[2]);
-            dataset.set(data[0], data[1], label);
+            //dataset.set(data[0], data[1], label);
+            dataTable.put(data[0], data[1], label);
+            colMap.put(data[0], data[1]);
         }
+        dataset=new SparseMatrix(userNumber,itemNumber,dataTable,colMap);
         return dataset;
     }
 }
