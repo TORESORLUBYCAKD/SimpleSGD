ffad91b7d019e53119e99ae402a94df611a48c90
TORESORLUBYCAKD <3326017670@qq.com>
2018-05-02 07:40:08 +0800

    Add RSVD

---
+++ src/RSVD.java
@@ -0,0 +1,87 @@
+import gzl.sgd.math.DenseMatrix;
+import gzl.sgd.math.DenseVector;
+import gzl.sgd.math.SparseMatrix;
+import java.util.Random;
+
+public class RSVD {
+
+	public static int numIterations=10;
+	public static int numFactors=96;
+	public static double lamda=0.02;
+	public static double lrate=0.001;
+	
+	public static int numUsers;
+	public static int numItems;
+	public static int numRates;
+	
+	public static SparseMatrix trainMatrix;
+	public static SparseMatrix testMatrix;
+	public static SparseMatrix TrainMatrix;
+	public static SparseMatrix TestMatrix;
+	public static DenseMatrix U;
+	public static DenseMatrix V;
+	
+	public static void main(String[] args) throws Exception{
+		
+		
+		numUsers=DataSet.userNumber;
+		numItems=DataSet.itemNumber;
+		TrainMatrix=new SparseMatrix(numUsers,numItems);
+		TrainMatrix=DataSet.readDataSet("dataset.txt");
+		TestMatrix=new SparseMatrix(numUsers,numItems);
+		TestMatrix=DataSet.readDataSet("testSet.txt");
+		U=new DenseMatrix(numUsers, numFactors);
+		V=new DenseMatrix(numFactors, numItems);
+		double initMean=0.0f;
+		double initStd=0.1f;
+		U.init(initMean,initStd);
+		V.init(initMean,initStd);
+		double preloss=100,curloss=10,cnt=0;
+		while(true) {
+			Random rand=new Random();
+			Random rand1=new Random();
+			Random rand2=new Random();
+			int userIdx=rand1.nextInt(numUsers-1);
+			int itemIdx=rand2.nextInt(numItems-1);	
+			DenseVector U_i=U.row(userIdx);
+			DenseVector V_j=V.column(itemIdx);
+			double r_ij=TrainMatrix.get(userIdx, itemIdx)-U_i.inner(V_j);
+			for(int k=0;k<numFactors;k++){
+				U.set(userIdx, k, U.get(userIdx, k)+lrate*(r_ij*V.get(k, itemIdx)-lamda*U.get(userIdx, k)));
+				V.set(k, itemIdx, V.get(k, itemIdx)+lrate*(r_ij*U.get(userIdx, k)-lamda*V.get(k, itemIdx)));
+			}
+			cnt++;
+			if(cnt%50000==0){
+				preloss=curloss;
+				curloss=Loss(TestMatrix,U,V);
+				if(Math.abs(preloss-curloss)<1E-8){
+					System.out.println("preloss: "+preloss+"curloss: "+curloss);
+					break;
+				}
+				System.out.println("Total Training Loss: "+Loss(TrainMatrix,U,V));
+				System.out.println("Total Testing Loss: "+curloss);
+			}			
+			//System.out.println("Total Testing Loss: "+Loss(B,U,V));
+		}
+		
+	}
+	
+	public static double Loss(SparseMatrix A,DenseMatrix U,DenseMatrix V) throws Exception{
+		DenseMatrix A_approx;
+		double loss=0;
+		double square=0;
+		double count=0;
+		A_approx=U.mult(V);
+		for(int i=0;i<A.numRows;i++){
+			for(int j=0;j<A.numColumns;j++){
+				if(A.get(i, j)!=0){
+					loss=A_approx.get(i, j)-A.get(i, j);
+					square=loss*loss;
+					count+=1;
+				}				
+			}
+		}
+		return square/count;
+	}
+
+}
